1e690de37808929e3ef7cf7df44a71452da7400a
Jon Friskics <jonfriskics@gmail.com>
2018-07-27 22:38:17 -0400

    module 4

---
+++ WishList/Models/AccountViewModels/LoginViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace WishList.Models.AccountViewModels
+{
+    public class LoginViewModel
+    {
+        [Required]
+        [EmailAddress]
+        public String Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public String Password { get; set; }
+
+        public LoginViewModel()
+        {
+        }
+    }
+}
